88a416929cb23fe808171686fe4b5741c2a1c303
MAYU75 <74301447+MAYU75@users.noreply.github.com>
2021-01-22 17:17:02 +0530

    Java program example of using this keyword to refer current class book's instance variable bookname.

---
+++ example1this.java
@@ -0,0 +1,24 @@
+class book
+{
+	//creating instance variable
+	String bookname;
+	//creating a constructor with parameters
+	public book(String bookname)
+	{
+		this.bookname=bookname;
+	}
+	//creating a method display
+	public void display()
+	{
+		System.out.println("Bookname: " + bookname);
+	}
+}
+//creating a class
+class example1this
+{
+	public static void main(String[] args)
+	{
+		book b1=new book("java");
+		b1.display();
+	}
+}
